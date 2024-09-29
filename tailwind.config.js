ebc16ab5ecdda6f4b2e892cd813acd336bb7a7ca
Vince Cedie Pino <vincecediepino@gmail.com>
2024-09-29 16:51:43 +0700

    add required font family, add fontawsome

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+      color: {
+        pink: "hsl(322,100%,66%)",
+        "light-pink": "hsl(321,100%,78%)",
+        "light-red": "hsl(0,100%,63%)",
+        "very-dark-cyan": "hsl(192,100%,9%)",
+        "very-pale-blue": "hsl(207,100%,98%)",
+      },
+    },
   },
   plugins: [],
 };
